f13109ae375018d1065d833e5691db3792b4ece9
asmau <alex.smau@gmail.com>
2021-10-18 16:04:59 +0200

    Improve the code comments and add new main file
    The comments of the newly added functions have been updated and
    now reflect reality.
    I have also added a new main matlab file. All our control code
    should go there.
    The matlab-UR5 API is now clean enough for the team to start working
    with it.

--- UR5_matlab_interface/UR5SerialAPI/read_actual_joint_positions.m
+++ UR5_matlab_interface/UR5SerialAPI/read_actual_joint_positions.m
@@ -1,36 +1,40 @@
 % t is the Socket Connection handle
 function P = read_actual_joint_positions(t)
 
-if t.BytesAvailable>0
-    fscanf(t,'%c',t.BytesAvailable);
-end
-fprintf(t,'(7)'); % task = 2 : reading task
-while t.BytesAvailable==0
-end
-rec = fscanf(t,'%c',t.BytesAvailable);
-rec
-if ~strcmp(rec(1),'[') || ~strcmp(rec(end),']')
-    error('robotpose read error')
-end
-rec(end) = ',';
-Curr_c = 1;
-for i = 1 : 6
-    C = [];
-    Done = 0;
-    while(Done == 0)
-        Curr_c = Curr_c + 1;
-        if strcmp(rec(Curr_c) , ',')
-            Done = 1;
-        else
-            C = [C,rec(Curr_c)];
+    if t.BytesAvailable>0
+        fscanf(t,'%c',t.BytesAvailable);
+    end
+    
+    % task = 7 : read_actual joint positions.
+    % Note: the value of 7 is hard-coded and was chosen at
+    % random.
+    fprintf(t,'(7)'); 
+    
+    while t.BytesAvailable==0
+    end
+    rec = fscanf(t,'%c',t.BytesAvailable);
+    rec
+    if ~strcmp(rec(1),'[') || ~strcmp(rec(end),']')
+        error('robotpose read error')
+    end
+    rec(end) = ',';
+    Curr_c = 1;
+    for i = 1 : 6
+        C = [];
+        Done = 0;
+        while(Done == 0)
+            Curr_c = Curr_c + 1;
+            if strcmp(rec(Curr_c) , ',')
+                Done = 1;
+            else
+                C = [C,rec(Curr_c)];
+            end
+        end
+        P(i) = str2double(C);   
+    end
+    for i = 1 : length(P)
+        if isnan(P(i))
+            error('robotpose read error (Nan)')
         end
     end
-    P(i) = str2double(C);   
-end
-for i = 1 : length(P)
-    if isnan(P(i))
-        error('robotpose read error (Nan)')
-    end
-end
-% P(1:3) = P(1:3)*1000; % converting to mm
 end
--- UR5_matlab_interface/UR5SerialAPI/set_joint_positions.m
+++ UR5_matlab_interface/UR5SerialAPI/set_joint_positions.m
@@ -1,32 +1,36 @@
-% Goal_Pose should be in mm and Orientation the rotation vector
-function P_new = set_joint_positions(t, P)
-if nargin ~= 2
-    error('error; not enough input arguments')
-% elseif nargin == 2
-%     P = Goal_Pose;
-% elseif nargin == 3
-%     P = [Goal_Pose,Orientation];
-end
-%P(1:3) = P(1:3) * 0.001; % Converting to meter
-P_char = ['(',num2str(P(1)),',',...
-    num2str(P(2)),',',...
-    num2str(P(3)),',',...
-    num2str(P(4)),',',...
-    num2str(P(5)),',',...
-    num2str(P(6)),...
-    ')'];
-success = '0';
-while strcmp(success,'0')
-    fprintf(t,'(8)'); % task = 1 : moving task
-    pause(0.01);% Tune this to meet your system
-    fprintf(t,P_char);
-    while t.BytesAvailable==0
-        %t.BytesAvailable
+% Tell the UR5 to move to the desired joint positions
+%
+% The funtion requires a socket pointer and an array
+% of 6 float numbers, each representing the the angle
+% radians of each joint.
+%
+% Input:
+% t - socket pointer (already opened) to the robot.
+% P - an array of 6 float numbers.
+function set_joint_positions(t, P)
+    if nargin ~= 2
+        error('error; not enough input arguments')
+
     end
-    success  = readrobotMsg(t);
-end
-if ~strcmp(success,'1')
-    error('error sending robot pose')
-end
-%pause(0.5)
-%P_new = readrobotpose(t);
+    P_char = ['(',num2str(P(1)),',',...
+        num2str(P(2)),',',...
+        num2str(P(3)),',',...
+        num2str(P(4)),',',...
+        num2str(P(5)),',',...
+        num2str(P(6)),...
+        ')'];
+    success = '0';
+    while strcmp(success,'0')
+        % task = 8 : ReadAndStoreJointPositionsFormSocket()
+        % Value of 8 was hard-coded and chosen at random.
+        fprintf(t,'(8)'); 
+        pause(0.01);% Tune this to meet your system
+        fprintf(t,P_char);
+        while t.BytesAvailable==0
+            %t.BytesAvailable
+        end
+        success  = readrobotMsg(t);
+    end
+    if ~strcmp(success,'1')
+        error('error sending robot pose')
+    end
